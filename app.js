caed56da514f1cced0aa56a7a78886056f1e57fe
mrbarbasa <mrbarbasa@gmail.com>
2014-09-09 17:55:07 -1000

    User may now submit a new post and upvote posts

--- app.js
+++ app.js
@@ -23,4 +23,21 @@ app.controller('MainCtrl', ['$scope', function($scope) {
       upvotes: 4
     }
   ];
+
+  $scope.addPost = function() {
+    if ($scope.title === '') {
+      return;
+    }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
 }]);
