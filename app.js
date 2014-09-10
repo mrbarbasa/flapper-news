08eb0969be5375b84856b10e56a77990e4c8ac91
mrbarbasa <mrbarbasa@gmail.com>
2014-09-09 21:07:59 -1000

    Implemented post details page and comments

--- app.js
+++ app.js
@@ -2,21 +2,47 @@ var app = angular.module('flapperNews', ['ui.router']);
 
 app.config(['$stateProvider', '$urlRouterProvider',
   function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state('home', {
+  $stateProvider
+    .state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
-    $urlRouterProvider.otherwise('home');
-  }
-])
+  $urlRouterProvider.otherwise('home');
+}])
 
 app.factory('posts', [function() {
   var o = {
     posts: []
   };
   return o;
+}]);
+
+app.controller('PostsCtrl', ['$scope', '$stateParams', 'posts',
+  function($scope, $stateParams, posts) {
+  $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function() {
+    if ($scope.body === '') {
+      return;
+    }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+
+  $scope.incrementUpvotes = function(comment) {
+    comment.upvotes += 1;
+  };
 }]);
 
 app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
@@ -51,7 +77,11 @@ app.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
     $scope.posts.push({
       title: $scope.title,
       link: $scope.link,
-      upvotes: 0
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
