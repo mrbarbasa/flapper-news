14a2d7ec3178ad60dddb4975f9c1786ab3b5ca50
mrbarbasa <mrbarbasa@gmail.com>
2014-09-10 18:54:18 -1000

    Implemented Express routes for posts and comments

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-
-/**
- * Module dependencies
- */
+// Models
+// These models must be registered before the var routes line.
+require('./models/Posts');
+require('./models/Comments');
+require('mongoose').connect('mongodb://localhost/news');
 
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-var mongoose = require('mongoose');
 
 var app = express();
 
-// models
-require('./models/Posts');
-require('./models/Comments');
-
-// db connection
-mongoose.connect('mongodb://localhost/news');
-
-// all environments
+// All environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -31,14 +24,30 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
+// Development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Routes
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+// Post routes
+app.get('/posts', routes.read);
+app.post('/posts', routes.create);
+// Retrieves post object from db and attaches it to req.post
+app.param('post', routes.load);
+app.get('/posts/:post', routes.retrieve);
+app.put('/posts/:post/upvote', routes.upvote);
+
+// Comment routes
+app.post('/posts/:post/comments', routes.commCreate);
+app.param('comment', routes.commLoad);
+app.get('/posts/:post/:comment', routes.commRetrieve);
+app.put('/posts/:post/:comment/upvote', routes.commUpvote);
+
+// Initiate server
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,112 @@
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
-/*
- * GET home page.
- */
+exports.index = function(req, res) {
+  res.render('index', { title: 'Flapper News' });
+};
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+// Routes for Posts
+
+exports.read = function(req, res, next) {
+  Post.find(function(err, posts) {
+    if (err) {
+      return next(err);
+    }
+    res.json(posts);
+  });
+};
+
+exports.create = function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    res.json(post);
+  });
+};
+
+exports.load = function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post) {
+    if (err) {
+      return next(err);
+    }
+    if (!post) {
+      return next(new Error("can't find post"));
+    }
+
+    req.post = post;
+    return next();
+  });
+};
+
+exports.retrieve = function(req, res, next) {
+  // Automatically loads all comments associated with the post
+  req.post.populate('comments', function(err, post) {
+    res.json(post);
+  });
+};
+
+exports.upvote = function(req, res, next) {
+  req.post.upvote(function(err, post) {
+    if (err) {
+      return next(err);
+    }
+    res.json(post);
+  });
+};
+
+
+// Routes for Comments
+
+exports.commCreate = function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+
+  comment.save(function(err, comment){
+    if (err) {
+      return next(err);
+    }
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if (err) {
+        return next(err);
+      }
+      res.json(comment);
+    });
+  });
+};
+
+exports.commLoad = function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment) {
+    if (err) {
+      return next(err);
+    }
+    if (!comment) {
+      return next(new Error("can't find comment"));
+    }
+
+    req.comment = comment;
+    return next();
+  });
+};
+
+exports.commRetrieve = function(req, res, next) {
+  res.json(req.comment);
+};
+
+exports.commUpvote = function(req, res, next) {
+  req.comment.upvote(function(err, comment) {
+    if (err) {
+      return next(err);
+    }
+    res.json(comment);
+  });
 };
